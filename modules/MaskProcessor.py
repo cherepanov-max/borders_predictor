1f2f8253256a0db13896cdf6745a731c6fef8b85
cherepanov-max <cherepanov@ku66.ru>
2020-04-15 10:02:56 +0500

    v 1.0

--- modules/MaskProcessor.py
+++ modules/MaskProcessor.py
@@ -5,6 +5,7 @@ import math
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from copy import deepcopy
+from skimage.morphology import square, binary_closing, remove_small_objects
 
 
 class MaskProcessor:
@@ -13,42 +14,74 @@ class MaskProcessor:
     # @var integer Шаг для расчитывания массива градусов
     DEGREE_STEP = 1
 
-
     # @var integer
     CLOSURE_RATIO = 0.5
 
+    # @var dict Пределы вариации для замыкания границ по проходам
+    VARIATION_LIMITS = {
+        1:0.45,
+        2:0.3
+    }
 
     # @var numpy array (2d) Маска изображения
     mask = []
 
+    # @var int номер прохода замыкания границ
+    closing_number = 1
 
     # @var numpy array (1d) Градусы, по которым будет просчитываться угол
     degreeArray = np.array([])
 
-    max_steps_to_closing = 0
-
+    # @var int Угол в градусах для поска обратного сильного изменения
+    max_steps_to_closing = None
 
     def __init__(self, maskPath):
         """   Конструктор   """
         # @var maskPath string - Путь к изображению маски
-
         self.mask = img_as_bool(io.imread(maskPath))
-        self.prepareDegreeArray()
-        self.max_steps_to_closing = int(len(self.degreeArray) / 4)
-        self.test(220, 180)
-        # self.test7(340, 120)
+        self.prepare_degree_array()
+        self.max_steps_to_closing = int(len(self.degreeArray) / 6)
 
+        self.draw_mask()
+        self.mask_morphology_close()
+        self.process_mask()
+        self.closing_number = 2
+        self.process_mask()
+        self.draw_mask()
 
-    def prepareDegreeArray(self):
+    def draw_mask(self):
+        """   Отобразить изображение маски   """
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        plt.close('all')
+
+    def process_mask(self):
+        """   Обработать маску   """
+        shape = self.mask.shape
+        for y in range(20, shape[0], 20):
+            for x in range(20, shape[1], 20):
+                if self.mask[y][x] == True:
+                    continue
+                print(y, x)
+                slice = self.prepareSliceForDot(y, x)
+                lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
+                if self.calculateVariationForLengthsArray(lengthsArray) > self.VARIATION_LIMITS[self.closing_number]:
+                    continue
+                derivativeArray = self.prepareDerivativeArray(lengthsArray)
+                new_lengths_array, new_derivative_array = self.process_dot_border(y, x, slice, lengthsArray, derivativeArray)
+                self.drawBorderInMask(y, x, new_lengths_array, slice)
+        self.mask_morphology_close()
+
+    def prepare_degree_array(self):
         """   Подготовить градусы, по которым будет просчитываться угол   """
         degree = 0
         while degree < 360:
             self.degreeArray = np.append(self.degreeArray, degree)
             degree += self.DEGREE_STEP
 
-
-    def moveToBorder(self, degree, step):
-        """   Определить ординату по градусу и абциссе   """
+    def move_to_border(self, degree, step):
+        """   Определить координаты по градусу и длине шага   """
         # @var degree int - градус
         # @var step int - номер щага
         # @return int - ордината
@@ -90,7 +123,6 @@ class MaskProcessor:
                     x = y / math.tan(rad)
         return y, x
 
-
     def prepareSliceForDot(self, dotY, dotX):
         """   Построить срез прострелов маски в определенной точке   """
         # @var dotY int - координата У
@@ -103,7 +135,7 @@ class MaskProcessor:
         for degree in self.degreeArray:
             step = 1
             while True:
-                deltaY, deltaX  = self.moveToBorder(degree, step)
+                deltaY, deltaX  = self.move_to_border(degree, step)
                 y = int(dotY + deltaY)
                 x = int(dotX + deltaX)
                 if x >= limitsX[1] or\
@@ -116,7 +148,6 @@ class MaskProcessor:
                     step += 1
             slice = np.concatenate((slice, np.array([[y, x]])))
         return slice
-
 
     def convertSliceDotsToLenths(self, dotY, dotX, slice):
         """   Конвертировать срез прострелов маски в определенной точке в массив длин  """
@@ -133,13 +164,14 @@ class MaskProcessor:
             lengthsArray = np.append(lengthsArray, length)
         return lengthsArray
 
-
     def calculateVariationForLengthsArray(self, lenthsArray):
         """   Конвертировать срез прострелов маски в определенной точке в массив длин  """
         # @var np.array lenthsArray массив длин до границ по градусам
         # @float вариация выборки
-
-        return lenthsArray.std() / lenthsArray.mean()
+        new_lenths_array = deepcopy(lenthsArray)
+        new_lenths_array.sort()
+        new_lenths_array = new_lenths_array[10:350]
+        return new_lenths_array.std() / new_lenths_array.mean()
 
     def prepareDerivativeArray(self, lengthsArray):
         """   Получить массив производных для массива длин """
@@ -168,6 +200,12 @@ class MaskProcessor:
         TEMP_DIAGRAM = 'data/output/temp_diagram.jpg'
 
         slice = (self.prepareSliceForDot(y, x))
+        lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
+        variation = self.calculateVariationForLengthsArray(lengthsArray)
+        if variation >= self.VARIATION_LIMIT_2:
+            return
+        derivativeArray = self.prepareDerivativeArray(lengthsArray)
+
         X = []
         Y = []
         for dot in slice:
@@ -178,18 +216,11 @@ class MaskProcessor:
         ax.scatter([x], [y], s=1, marker=",")
         ax.imshow(self.mask)
         plt.savefig(TEMP_MASK, dpi=300)
-        # plt.show()
-        # print(img)
-
-        lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
-        derivativeArray = self.prepareDerivativeArray(lengthsArray)
 
         fig, ax = plt.subplots()
         ax.bar(self.degreeArray, lengthsArray)
         ax.plot(self.degreeArray, derivativeArray, color='red')
         plt.savefig(TEMP_DIAGRAM, dpi=300)
-
-        variation = self.calculateVariationForLengthsArray(lengthsArray)
 
         D = io.imread(TEMP_DIAGRAM)
         M = io.imread(TEMP_MASK)
@@ -199,7 +230,7 @@ class MaskProcessor:
         # fig, ax = plt.subplots()
         # io.imshow(common)
         finalPath = 'data/collection'
-        if variation < 0.45:
+        if variation < self.VARIATION_LIMIT_2:
             finalPath = finalPath + '/good/common_' + str(y) + '_' + str(x) + 'var-' + str(round(variation*100, 0)) + '.jpg'
         else:
             finalPath = finalPath + '/bad/common_' + str(y) + '_' + str(x) + 'var-' + str(round(variation*100, 0)) + '.jpg'
@@ -208,11 +239,11 @@ class MaskProcessor:
 
     def close_border(self, y, x, lengthsArray, derivativeArray, start, stop):
         end_lengths = 1 # длина массива перед и после интерполируемыми значениями
-        degree_array_length = self.degreeArray # длина массива длин
+        degree_array_length = len(self.degreeArray) # длина массива длин
 
         # инкрементруем стоп. чтобы не цеплять последнее значение
         stop += 1
-        if stop == len(degree_array_length):
+        if stop == degree_array_length:
             stop = 0
 
         # индексы перед и после интерполируемыми значениями
@@ -220,16 +251,16 @@ class MaskProcessor:
         for i in range(start - end_lengths, start):
             index = i
             if index < 0:
-                index += len(degree_array_length)
-            if index > len(degree_array_length):
-                index -= len(degree_array_length)
+                index += degree_array_length
+            if index > degree_array_length:
+                index -= degree_array_length
             indexes = np.append(indexes, index)
         for i in range(stop, stop + end_lengths):
             index = i
             if index < 0:
-                index += len(degree_array_length)
-            if index > len(degree_array_length):
-                index -= len(degree_array_length)
+                index += degree_array_length
+            if index > degree_array_length:
+                index -= degree_array_length
             indexes = np.append(indexes, index)
 
         # значения Х для апроксимации
@@ -238,7 +269,7 @@ class MaskProcessor:
 
         # длина массива для интерполяции
         if range_length < 0:
-            range_length += len(degree_array_length)
+            range_length += degree_array_length
         x_data = np.concatenate((x_data, [i for i in range(end_lengths + range_length, range_length + 2*end_lengths)]))
 
         # значения Y для апроксимации
@@ -249,14 +280,10 @@ class MaskProcessor:
         popt, pcov = curve_fit(linear, x_data, y_data)
         a, b = popt
 
-        print(start, stop)
-        print(indexes)
-        print(x_data)
         for numb, i in enumerate(range(start + 1, start + range_length)):
             index = i
-            if i > len(degree_array_length):
-                index -= len(degree_array_length)
-            print(numb + end_lengths, index)
+            if i >= degree_array_length:
+                index -= degree_array_length
             length_value = linear(numb + end_lengths, a, b)
             lengthsArray[int(index)] = round(length_value, 0)
         return lengthsArray, self.prepareDerivativeArray(lengthsArray)
@@ -267,10 +294,43 @@ class MaskProcessor:
             stop = False
             if derivativeArray[index] > limit:
                 stop = self.find_stop_by_outburst(index, derivativeArray)
-                if stop != False:
+                if stop != False and self.closing_number == 1:
+                    print('close border', index, stop)
                     lengthsArray, derivativeArray = self.close_border(y, x, deepcopy(lengthsArray), deepcopy(derivativeArray), index, stop)
+                if stop == False and self.closing_number == 2:
+                    stop = self.find_stop_by_one_deviation(index, deepcopy(lengthsArray))
+                    if stop != False:
+                        print('close border', index, stop)
+                        lengthsArray, derivativeArray = self.close_border(y, x, deepcopy(lengthsArray), deepcopy(derivativeArray), index, stop)
         return lengthsArray, derivativeArray
 
+    def drawBorderInMask(self, y, x, lengthsArray, slice):
+        # new_slice = []
+        for i, degree in enumerate(self.degreeArray):
+            border_y = y + math.sin(degree * math.pi / 180) * lengthsArray[i]
+            border_y = int(round(border_y))
+            if border_y > self.mask.shape[0]:
+                border_y = self.mask.shape[0]
+            elif border_y < 0:
+                border_y = 0
+            border_x = x + math.cos(degree * math.pi / 180) * lengthsArray[i]
+            border_x = int(round(border_x))
+            if border_x > self.mask.shape[1]:
+                border_x = self.mask.shape[1]
+            elif border_x < 0:
+                border_x = 0
+            self.mask[border_y-1:border_y+1, border_x-1:border_x+1] = True
+            # new_slice.append([border_y, border_x])
+        # new_slice = np.array(new_slice)
+        # odl_slice = np.array(slice)
+
+        # fix, ax = plt.subplots()
+        # ax.scatter(new_slice[:, 1], new_slice[:, 0])
+        # ax.scatter(odl_slice[:, 1], odl_slice[:, 0])
+        #
+        # ax.bar(self.degreeArray, lengthsArray)
+        # plt.show()
+        # exit()
 
     def find_stop_by_outburst(self, index, derivativeArray):
         stop_index = index + self.max_steps_to_closing # когда останавливаемся
@@ -286,7 +346,104 @@ class MaskProcessor:
             index += 1
         return False
 
-    def test(self, y, x):
+    def find_stop_by_one_deviation(self, index, legths_array):
+        degree_array_length = len(self.degreeArray)  # длина массива углов
+        filter_size = 3
+
+        legths_array = np.roll(legths_array, degree_array_length - index)
+        legths_array = np.append(legths_array[-filter_size:], legths_array)
+
+        stop = False
+        was_max = False
+        previous = legths_array[:filter_size + 1].mean()
+        for i in range(filter_size + 1, degree_array_length + filter_size):
+            current = legths_array[i - filter_size:i + 1].mean()
+            if was_max == False:
+                if current < previous:
+                    was_max = True
+            else:
+                if current > previous:
+                    stop = i
+                    break
+            previous = current
+        if stop == False:
+            return False
+        else:
+            stop += index
+        if stop >= degree_array_length:
+            stop -= degree_array_length
+        return stop
+
+    def mask_morphology_close(self):
+        self.mask = binary_closing(self.mask, square(25))
+        self.mask = remove_small_objects(self.mask)
+
+    def test(self):
+        self.closing_number = 2
+        # y, x = 60, 340
+        # slice = self.prepareSliceForDot(y, x)
+        # lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
+        # derivativeArray = self.prepareDerivativeArray(lengthsArray)
+        # new_lengths_array, new_derivative_array = self.process_dot_border(y, x, slice, lengthsArray, derivativeArray)
+        # self.drawBorderInMask(y, x, new_lengths_array, slice)
+        # fig, ax = plt.subplots()
+        # ax.bar(self.degreeArray, lengthsArray)
+        # ax.plot(self.degreeArray, derivativeArray, color='red')
+        # plt.show()
+        # exit()
+
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        shape = self.mask.shape
+        for y in range(20, shape[0], 20):
+            for x in range(20, shape[1], 20):
+                if self.mask[y][x] == True:
+                    continue
+                print(y, x)
+                slice = self.prepareSliceForDot(y, x)
+                lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
+                if self.calculateVariationForLengthsArray(lengthsArray) > self.VARIATION_LIMIT_2:
+                    continue
+                derivativeArray = self.prepareDerivativeArray(lengthsArray)
+                new_lengths_array, new_derivative_array = self.process_dot_border(y, x, slice, lengthsArray, derivativeArray)
+                self.drawBorderInMask(y, x, new_lengths_array, slice)
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        self.mask_morphology_close()
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        # io.imsave('data/output/mask_with_borders_1.jpg', img_as_ubyte(self.mask))
+        # plt.close('all')
+    def test9(self):
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        shape = self.mask.shape
+        for y in range(20, shape[0], 20):
+            for x in range(20, shape[1], 20):
+                if self.mask[y][x] == True:
+                    continue
+                print(y, x)
+                slice = self.prepareSliceForDot(y, x)
+                lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
+                if self.calculateVariationForLengthsArray(lengthsArray) > self.VARIATION_LIMIT_1:
+                    continue
+                derivativeArray = self.prepareDerivativeArray(lengthsArray)
+                new_lengths_array, new_derivative_array = self.process_dot_border(y, x, slice, lengthsArray, derivativeArray)
+                self.drawBorderInMask(y, x, new_lengths_array, slice)
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        plt.show()
+        self.mask = binary_closing(self.mask, square(25))
+        self.mask = remove_small_objects(self.mask)
+        fig, ax = plt.subplots()
+        plt.imshow(self.mask)
+        io.imsave('data/output/mask_with_borders_1.jpg', img_as_ubyte(self.mask))
+        plt.close('all')
+    def test8(self, y, x):
         slice = self.prepareSliceForDot(y, x)
         lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
         # fig, ax = plt.subplots()
@@ -296,12 +453,16 @@ class MaskProcessor:
             return
         derivativeArray = self.prepareDerivativeArray(lengthsArray)
         new_lengths_array, new_derivative_array = self.process_dot_border(y, x, slice, lengthsArray, derivativeArray)
+        self.drawBorderInMask(y, x, new_lengths_array, slice)
+
+        # fig, ax = plt.subplots()
+        # ax.bar(self.degreeArray, lengthsArray)
+        # ax.plot(self.degreeArray, new_lengths_array, color='red')
+        # plt.show()
 
         fig, ax = plt.subplots()
-        ax.bar(self.degreeArray, lengthsArray)
-        ax.plot(self.degreeArray, new_lengths_array, color='red')
+        plt.imshow(self.mask)
         plt.show()
-
     def test7(self, y, x):
 
         slice = (self.prepareSliceForDot(y, x))
@@ -344,11 +505,12 @@ class MaskProcessor:
         # variation = self.calculateVariationForLengthsArray(lengthsArray)
         print(derivativeArray)
     def test2(self):
-        y, x, = 400, 270
+        y, x, = 140, 640
+
         slice = (self.prepareSliceForDot(y, x))
         lengthsArray = self.convertSliceDotsToLenths(y, x, slice)
         derivativeArray = self.prepareDerivativeArray(lengthsArray)
-
+        print(self.calculateVariationForLengthsArray(lengthsArray))
         fig, ax = plt.subplots()
         ax.bar(self.degreeArray, lengthsArray)
         ax.plot(self.degreeArray, derivativeArray, color='red')
@@ -372,7 +534,7 @@ class MaskProcessor:
         steps = 50
 
         for i in range(1, steps):
-            x, y = self.moveToBorder(300, i)
+            x, y = self.move_to_border(300, i)
             X.append(x)
             Y.append(y)
 
@@ -381,7 +543,7 @@ class MaskProcessor:
         plt.show()
     def test5(self):
         for degree in self.degreeArray:
-            y, x = self.moveToBorder(degree, 5)
+            y, x = self.move_to_border(degree, 5)
 
             fig, ax = plt.subplots()
             ax.set(xlim=(-6, 6), ylim=(-6, 6))
